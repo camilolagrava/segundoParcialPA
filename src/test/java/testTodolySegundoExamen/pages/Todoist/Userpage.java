99658e9442ea78833a2991cd2cfd4e0561d3d9c9
Camilo <camilolagrava@gmail.com>
2022-03-10 08:42:25 -0400

    Final commit

--- src/test/java/testTodolySegundoExamen/pages/Todoist/Userpage.java
+++ src/test/java/testTodolySegundoExamen/pages/Todoist/Userpage.java
@@ -7,5 +7,5 @@ import org.openqa.selenium.By;
 public class Userpage {
     public Image userImage = new Image(By.xpath("//img[@class='user_avatar big settings_avatar']"));
     // Menu desplegable
-    public Button logouthButton = new Button(By.id("id-15s3pj-20"));
+    public Button logouthButton = new Button(By.xpath("//span[text()='Log out']"));
 }
--- src/test/java/testTodolySegundoExamen/test/Todoist/LogouthTest.java
+++ src/test/java/testTodolySegundoExamen/test/Todoist/LogouthTest.java
@@ -5,13 +5,14 @@ import org.junit.jupiter.api.Test;
 import pagesTodoist.LoginPage;
 import pagesTodoist.MainPage;
 import pagesTodoist.PersonalPage;
+import testTodolySegundoExamen.pages.Todoist.Userpage;
 
 public class LogouthTest extends BaseTodoist {
 
     LoginPage loginPage = new LoginPage();
     MainPage mainPage = new MainPage();
     PersonalPage personalPage = new PersonalPage();
-    
+    Userpage userpage =new Userpage();
     String email = "camilolagrava@gmail.com";
     String pass = "contrasenna123";
 
@@ -30,7 +31,12 @@ public class LogouthTest extends BaseTodoist {
 
     @Test
     public void logouthTest() throws InterruptedException{
-
+        loginTest();
+        userpage.userImage.click();
+        userpage.logouthButton.click();
+        Thread.sleep(2000);
+        Assertions.assertTrue(mainPage.imagenInicioDeSecion.isControlDisplayed(),
+                "No se puedo desloggear");
     }
 
 
